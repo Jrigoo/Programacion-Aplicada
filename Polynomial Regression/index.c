818a152bd177358e47dc239c5a21a9909ea4b00d
Juan Riquelme <pk.juanr@gmail.com>
2022-04-08 12:24:35 -0500

    :zap: Read .csv and change functions

--- Polynomial Regression/index.c
+++ Polynomial Regression/index.c
@@ -3,23 +3,33 @@
 # include <string.h>
 # include<math.h>
 
-void printMatrix(double matrix[][20],int rows,int columns){
+#define DATA "gps.csv"
+
+void printMatrix(double matrix[][100],int rows,int columns){
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < columns; j++){
-            printf("%f ",matrix[i][j]);
+            printf("%lf ",matrix[i][j]);
         }
         printf("\n");         
     }
 }
 
-double **multiply(double mat1[][20], double mat2[][20],int row1,int row2,int col2){
+void printArray(double values[],int n){
+    for (int i = 0; i < n; i++){
+        printf("%f",values[i]);
+    }
+    printf("\n");  
+}
+
+double **multiply(double mat1[][100], double mat2[][100],int row1,int row2,int col2){
     double **matrix;
-    matrix = malloc(sizeof(double) * row1);
+    matrix = malloc(sizeof(double) * 100);
      
-    for(int i = 0; i < row1; i++) {
-        matrix[i] = malloc(sizeof(double) * col2);
+    for(int i = 0; i < 100; i++) {
+        matrix[i] = malloc(sizeof(double) * 100);
     }
 
+    /* Matrix resultante (row1 x col1) * (row2 x col2) -> row1 x col2*/
 	for (int i = 0; i < row1; i++) {
 		for (int j = 0; j < col2; j++) {
 			matrix[i][j] = 0;
@@ -31,7 +41,7 @@ double **multiply(double mat1[][20], double mat2[][20],int row1,int row2,int col
     return matrix;
 }
 
-double **transpose(double mat1[][20],int rows,int columns){
+double **transpose(double mat1[][100],int rows,int columns){
     double **matrix;
     matrix = malloc(sizeof(double) * columns);
      
@@ -46,13 +56,13 @@ double **transpose(double mat1[][20],int rows,int columns){
     return matrix;
 }
 
-double **inverse(double matrix[][20],int order){
-    double adj[20][20],ratio;
+double **inverse(double matrix[][100],int order){
+    double adj[100][100],ratio;
     double **XInv;
 
-    XInv = malloc(sizeof(double) * order);
-    for(int i = 0; i < order; i++) {
-        XInv[i] = malloc(sizeof(double) * order);
+    XInv = malloc(sizeof(double) * 100);
+    for(int i = 0; i < 100; i++) {
+        XInv[i] = malloc(sizeof(double) * 100);
     }
     
 	for(int i=0;i<order;i++){
@@ -100,22 +110,17 @@ double **inverse(double matrix[][20],int order){
     return XInv;
 }
 
-int main(){
-    /* Declaramos nuestra variable independiente y puntos que se nos brindó */
+double **polynomialRegression(double x[100],double Y[][100],int grado,int n){
     double ratio;
-    int grado = 4;
 	int order = grado + 1;
-    double x[8] = {1,2,3,4,5,6,7,8};
-    double Y[8][1] = {5.74, 66.06, 301.46000000000004, 903.9399999999999, 2137.5, 4338.139999999999, 7913.86, 13344.66};
-    size_t n = sizeof(x) / sizeof(double);
 
-    double X[20][20];
-    double XT[20][20];
-    double XRes[20][20];
-    double XYRes[20][20];
-    double adj[20][20];
-    double XInv[20][20];
-    double beta[20][20];
+    double X[100][100];
+    double XT[100][100];
+    double XRes[100][100];
+    double XYRes[100][100];
+    double adj[100][100];
+    double XInv[100][100];
+    double beta[100][100];
 
     double **XTP;
     double **XPRes;
@@ -123,37 +128,36 @@ int main(){
     double **XPInv;
     double **bp;
 
-    printf("-------------------------------------------\n");
-    printf("Matriz X\n");
+    //printf("-------------------------------------------\n");
+    //printf("Matriz X\n"); //n x order (46 x 6)
     for (int i = 0; i < n; i++){
         for (int j = 0; j < order; j++){
             X[i][j] = powf(x[i],j);
         }
     }
-    printMatrix(X,n,order);
-    printf("-------------------------------------------\n");
-
-    printf("Transponemos la matriz X\n");
-    XTP = transpose(X,n,order);
+    //printMatrix(X,n,order);
+    //printf("-------------------------------------------\n");
+    //printf("Transponemos la matriz X\n");
+    XTP = transpose(X,n,order); /* 6 x 46 */
     for (int i = 0; i < order; i++) {
         for (int j = 0; j < n; j++) {
             XT[i][j] = XTP[i][j];
         }
     } 
-    printMatrix(XT,order,n);
-    printf("-------------------------------------------\n");
-
-    printf("Multiplicamos XT*X ->>> XRes\n");
-    XPRes = multiply(XT,X,order,n,order); //m1,m2,fila1,fila2,columna2
+    //printMatrix(XT,order,n);
+    //printf("-------------------------------------------\n");
+    //printf("Multiplicamos XT*X ->>> XRes\n");
     for (int i = 0; i < order; i++) {
-        for (int j = 0; j < order; j++) {
-            XRes[i][j] = XPRes[i][j];
-        }
-    } 
-    printMatrix(XRes,order,order);
-    printf("-------------------------------------------\n");;
-
-    printf("Multiplicamos XT*Y ->>> XYRes\n");
+		for (int j = 0; j < order; j++) {
+			XRes[i][j] = 0;
+			for (int k = 0; k < n; k++) {
+				XRes[i][j] += XT[i][k] * X[k][j];
+			}
+		}
+	} 
+    //printMatrix(XRes,order,order);
+    //printf("-------------------------------------------\n");;
+    //printf("Multiplicamos XT*Y ->>> XYRes\n");
     for (int i = 0; i < order; i++) {
 		for (int j = 0; j < 1; j++) {
 			XYRes[i][j] = 0;
@@ -162,30 +166,133 @@ int main(){
 			}
 		}
 	} 
-    printMatrix(XYRes,order,1);
-    printf("-------------------------------------------\n");
+    //printMatrix(XYRes,order,1);
+    //printf("-------------------------------------------\n");
+    //printf("Invertimos XYRes -> XInv\n");
+	for(int i=0;i<order;i++){
+		for(int j=0;j<order;j++){
+		    adj[i][j] = XRes[i][j];
+		}
+	}
+	for(int i=0;i<order;i++){
+		for(int j=0;j<order;j++){
+			if(i==j){
+				adj[i][j+order] = 1;
+			}
+			else{
+				adj[i][j+order] = 0;
+			}
+		}
+	}
+	for(int i=0;i<order;i++){
+		if(adj[i][i] == 0.0){
+			//printf("Mathematical Error!");
+			return 0;
+		}
+		for(int j=0;j<order;j++){
+			if(i!=j){
+				ratio = adj[j][i]/adj[i][i];
+				for(int k=1;k<2*order;k++)
+				{
+					adj[j][k] = adj[j][k] - ratio*adj[i][k];
+				}
+			}
+		}
+	}
+	for(int i=0;i<order;i++){
+		for(int j=order;j<2*order;j++){
+		adj[i][j] = adj[i][j]/adj[i][i];
+		}
+	}
+	for(int i=0;i<order;i++){
+        int c = 0;
+		for(int j=order;j<2*order;j++){
+            XInv[i][c] = adj[i][j];
+            c++;
+		}
+	}
+    //printMatrix(XInv,order,order);
+    //printf("-------------------------------------------\n");
+    //printf("Obtenemos coeficientes beta\n");
+    bp = multiply(XInv,XYRes,order,order,1);
+    /* for (int i = 0; i < order; i++) {
+		for (int j = 0; j < 1; j++) {
+			beta[i][j] = 0;
+			for (int k = 0; k < order; k++) {
+				beta[i][j] += XInv[i][k] * XYRes[k][j];
+			}
+		}
+	} 
+    printMatrix(beta,order,1); */
+    return bp;
+}
 
-    printf("Invertimos XYRes -> XInv\n");
-    XPInv = inverse(XRes,order);
-    for (int i = 0; i < order; i++) {
-        for (int j = 0; j < order; j++) {
-            XInv[i][j] = XPInv[i][j];
+int countLines(){
+    FILE * fp = fopen(DATA,"r");
+	if(!fp){ 
+		printf("\n ERROR! \n\n No se puede abrir el archivo. \n");
+		return 0;
+	}
+	int ch = 0; int n = 0; //detectar el numero de lineas del archivo
+	while(!feof(fp)){ 		//determinar el numero de datos.
+		ch = fgetc(fp);
+		if (ch == '\n'){
+			n++;
+		}
+	}
+    return n;
+}
+
+double **readFile(int n){
+    FILE * fp = fopen(DATA,"r");
+    double **csvData;
+	int x = 0; int y = 0;
+
+    csvData = malloc(sizeof(double) * n);
+    for(int i = 0; i < n; i++) {
+        csvData[i] = malloc(sizeof(double) * n);
+    }
+
+	while(x<n){		//ciclo while para guardar los datos del csv a nuestra matriz
+		fscanf(fp, "%lf", &csvData[x][y]); 	
+        fscanf(fp,",");
+        y++;
+        fscanf(fp, "%lf", &csvData[x][y]); 	
+        x++;
+        y = 0;
+	}
+    return csvData;
+}
+
+
+int main(){
+    int n = countLines();
+    double **gpsData;
+    double x[100];
+    double Y[100][100];
+
+    gpsData = readFile(n);
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < 2; j++){
+            if (j == 0){
+                x[i] = gpsData[i][j];
+            }else{
+                Y[i][0] = gpsData[i][j];
+            }
         }
-    } 
-    printMatrix(XInv,order,order);
-    printf("-------------------------------------------\n");
-
-    printf("Obtenemos coeficientes beta\n");
-    bp = multiply(XInv,XYRes,5,5,1);
-    for (int i = 0; i < order; i++) {
-        for (int j = 0; j < 1; j++) {
-            beta[i][j] = bp[i][j];
-        }
-    } 
-    printMatrix(beta,order,1);
-    printf("------------------------------\n");
-
-    printf("Nuestra funcion termina siendo: f(x) = %.2f + (%.2fx) + (%.2fx^2) + (%.2fx^3) + (%.2fx^4)",bp[0][0],bp[1][0],bp[2][0],bp[3][0],bp[4][0]);
+    }
     
+    int grado;
+    printf("Ingresa el grado de la matriz: ");
+    scanf("%d",&grado);
+
+    double **bp;
+    bp = polynomialRegression(x,Y,grado,n);
+    printf("Coeficientes Beta ------------->");
+    for (int i = 0; i < grado+1; i++){
+        printf("Para x^%d: %f",i,bp[i][0]);
+        printf("\n");
+    }
+
     return 0;
 }
