26318ce7c4dd52a60b80e9fc1b98fdc82517088f
Juan Riquelme <pk.juanr@gmail.com>
2022-05-06 12:41:35 -0500

    :tada: First Commit

---
+++ Python/main.py
@@ -0,0 +1,10 @@
+import serial
+gps = serial.Serial("COM14",baudrate=9600)
+while True:
+    line  = str(gps.readline())
+    if "$GPGGA" in line:
+        data = line.split(",")
+        latitud = data[2].strip()
+        longitud = data[4].strip()
+        satelites = data[7].strip()
+        print(f"Latitud: {latitud} Longitud: {longitud} # de satelites: {satelites}")
